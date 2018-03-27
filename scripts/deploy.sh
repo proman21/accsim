1730a71a1bab5deaeec2c39371073eaf23c0dc4f
Adam Jacobus <adam.jacobus@uqconnect.edu.au>
2018-03-27 13:11:30 +1000

    Add deployment script to repo
    Deployment script relies on using public key identification to authorise with the server. You must have set your SSH public key in authorized_keys on both servers

---
+++ scripts/deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Issue warning about deployment
+echo 'WARNING! Deployment is not meant to be used to test active development builds.'
+echo 'Use "npm run serve" to use a local web server with hot-reloading.'
+
+base_dir=$(dirname $BASH_SOURCE)
+deploy_file=$base_dir/../deploy_config
+
+# Find ssh config for the deployment. If not found, build a new one from
+# a template
+if [[ ! -f $deploy_file ]]; then
+  echo "No deployment configuration found. Making one now."
+
+  read -er -p "Enter UQ username: " username
+  username=${username//[$'\t\r\n']}
+
+  eval "cat > $deploy_file <<EOF
+# This configuration uses a proxy jump to get through when the computer
+# isn't in the UQ network
+Host accountable
+  HostName deco3800-accountable.zones.eait.uq.edu.au
+  User ${username}
+  ProxyJump ${username}@remote.labs.eait.uq.edu.au
+EOF"
+
+  echo "Configuration created at $deploy_file"
+fi
+
+# Trigger a build
+npm run build &>/dev/null || { exit 1; }
+
+# SCP the build into the server
+scp -r -F $deploy_file $base_dir/../dist/. accountable:/var/www/htdocs
